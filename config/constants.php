0928f4e4a90f00850c4f019405d12a6afb2e3678
Saviour Dagadu <savicane@gmail.com>
2022-06-07 13:29:44 +0000

    Made chages to config/constants.php to improve the url to the site

--- config/constants.php
+++ config/constants.php
@@ -4,11 +4,10 @@
 
 //Create Constants to Store Non Repeating Values
 define('SITEURL', 'http://localhost:8082/food-order/');
-define('LOCALHOST', 'sql102.epizy.com');
-define('DB_USERNAME', 'epiz_31778374');
-define('DB_PASSWORD', '
-Bzp5WSxqYwEip');
-define('DB_NAME', 'epiz_31778374_foodorder');
+define('LOCALHOST', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'food-order');
 
 //3. Execute Query and Save Data in Database
 $con = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(); //Database COnnection
