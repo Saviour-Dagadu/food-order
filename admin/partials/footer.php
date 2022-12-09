62cb7454cc2040c6c7e067786240f715817ade08
Saviour Dagadu (Savi) <79539570+Saviour-Dagadu@users.noreply.github.com>
2022-12-09 04:28:06 +0000

    Adding chnages made to admin/partials/footer.php

--- admin/partials/footer.php
+++ admin/partials/footer.php
@@ -2,7 +2,7 @@
 <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
 <div class="footer">
             <div class="wrapper text-center">
-                <p class="text-center"> &copy; 2021 | All rights reserved | Food House | Developed By - <a href="https://objective-brown-fa0d45.netlify.app/ "> Savi </a> </p>
+                <p class="text-center"> &copy; 2021 | All rights reserved | Food House | Developed By - <a href="http://savytechdeveloper.netlify.app/"> Savi </a> </p>
             </div>
         </div>
         <!-----Footer Section Ends----->
